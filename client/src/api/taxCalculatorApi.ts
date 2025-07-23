1d46e39182b66f03f135d4598322f96692c7f370
Negar Ashouri <negara@ex-situ.com>
2025-07-23 01:14:22 -0400

    added comments on the api fetch code

--- client/src/api/taxCalculatorApi.ts
+++ client/src/api/taxCalculatorApi.ts
@@ -1,7 +1,14 @@
 import { TaxBracketsResponse } from "../types";
 
 const BASE_URL = "http://localhost:5001/tax-calculator";
-
+/**
+ * Fetch tax bracket data from the backend API.
+ *
+ * @param year Optional tax year to fetch brackets
+ *             If omitted, fetches default tax brackets.
+ * @returns A Promise resolving to the tax brackets response.
+ * @throws Throws an error if the network request fails or response is not OK.
+ */
 export const fetchTaxBrackets = async (
     year?: number
   ): Promise<TaxBracketsResponse> => {
